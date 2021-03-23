1e4d7d5465cb028a5e1bf741fe3efa230d40f132
victormedel <medel.victor@gmail.com>
2021-03-23 10:20:41 -0700

    config reference rename

--- tweepy_prototype.py
+++ tweepy_prototype.py
@@ -5,8 +5,8 @@ with open("config.yml", "r") as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
 # Authenticate to Twitter
-auth = tweepy.OAuthHandler(cfg['twitter_auth']['consumer_api_key'], cfg['twitter_auth']['consumer_api_secret_key'])
-auth.set_access_token(cfg['twitter_auth']['api_key'], cfg['twitter_auth']['api_secret_key'])
+auth = tweepy.OAuthHandler(cfg['twitter_auth']['api_key'], cfg['twitter_auth']['api_secret_key'])
+auth.set_access_token(cfg['twitter_auth']['access_token'], cfg['twitter_auth']['secrete_access_token'])
 
 # Create API object
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
