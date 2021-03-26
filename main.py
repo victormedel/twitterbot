062a20b90bd244191d3519a23e630ec7fc6b0f2b
victormedel <medel.victor@gmail.com>
2021-03-26 14:17:29 -0700

    get current time

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import time
 import threading
+from datetime import datetime
 from w3w_tweet import get_trending, get_loc, map_generator, twitter_post
 
 WAIT_SECONDS = 43200
 
 def twitter_post():
+    # Establish current date and time
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    
+    # Post to Twitter
     trd_words = get_trending("United States")
     sugg_words, nearest_loc, country, latitude, longitude = get_loc(trd_words)
     twitter_post(sugg_words, nearest_loc , country, latitude, longitude)
+
+    print("Posted On = ", dt_string)
 
 
 if __name__ == "__main__":
