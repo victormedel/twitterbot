6ed40fc3276ca4a6d999d00196ac5caaeda3126b
victormedel <medel.victor@gmail.com>
2021-03-28 18:11:31 -0700

    logging update

--- bot/w3w_tweet.py
+++ bot/w3w_tweet.py
@@ -26,7 +26,7 @@ WAIT_SECONDS = 60 # 7200
 
 
 def get_trending(t_api, loc):
-    
+    logger.info('Retrieving and parsing trending topics into seperate words')
     trending_words = [] # Empty List to Hold Words
 
     # Trends for Specific Country
@@ -44,41 +44,51 @@ def get_trending(t_api, loc):
                 trending_words.append(value)
     
     # Word List Cleanup
+    logger.info('Cleaning up word list')
     trending_words = [i for i in trending_words if len(i) > 2] # remove single characters entries
     trending_words = [x.lower() for x in trending_words] # lowercase all list
     
+    logger.info('Removing words that do no appear in the english dictionary')
     trending_words = word_check(trending_words) # remove any words in list not in english dictionary
-
+    
+    logger.info('Randomly selecting three words from the composed list')
     three_words = random.sample(trending_words, 3) # randomly select three words
     three_word_str = '.'.join(three_words) # join list items
 
+    logger.info('Returning three word list')
     return three_word_str
 
 
 def get_loc(w_api, words):
     # Create What3Words API object
+    logger.info('Creating What3Words API Object')
     geocoder = what3words.Geocoder(w_api)
 
     # Get nearest location based on what3words word suggestion
     result = geocoder.autosuggest(words)
 
     # Get values from item in set ranked #1
+    logger.info('Extracting information from What3Words object')
     country_abv = result['suggestions'][0]['country']
     nearest_loc = result['suggestions'][0]['nearestPlace']
     sugg_words = result['suggestions'][0]['words']
 
     # Get latitude and longitude for suggested words
+    logger.info('Getting latitude and longitude for the three words suggested')
     coord = geocoder.convert_to_coordinates(sugg_words)
     latitude = str(coord['coordinates']['lat'])
     longitude = str(coord['coordinates']['lng'])
 
+    logger.info('Converting country abbreviation to full country name')
     country = pycountry.countries.get(alpha_2=country_abv)
 
+    logger.info('Returning location data')
     return sugg_words, nearest_loc , country.name, latitude, longitude
 
 
 def map_generator(g_api, latitude, longitude):
     # get image map of coordinates - should be a function
+    logger.info('Retrieving map image')
     center = latitude + ',' + longitude
     zoom = 16
     size = "800x400"
@@ -89,31 +99,38 @@ def map_generator(g_api, latitude, longitude):
     req = requests.get(url)
 
     # Store image file to be used for post
+    logger.info('Storing map image to used for Twitter post')
     file_name = '..\map_img.png'
     f = open(file_name, 'wb') 
     f.write(req.content) 
     f.close()
 
+    logger.info('Retrieving image size')
     file_stats = os.stat(file_name)
     file_size = file_stats.st_size/1024
-    
-    print('File Size: ', file_size)
+    logger.info('File size: ' + str(file_size))
 
+    logger.info('Returning file name for map')
     return file_name
 
 
 def twitter_post(t_api, file_name, trd_words, sugg_words, nearest_loc , country, latitude, longitude):
+    logger.info('Using all gathered and generated data to compose and publish post')
     
     # Post Text
+    logger.info('Generating text for post')
     status =  emoji.emojize(':fire:') + ' 3 random words from trending topics in the US today are:\n' + \
                trd_words + '\n\n' + \
-               emoji.emojize(':gear:') + ' Slightly modifying these words to:\n' + \
+               emoji.emojize(':gear:') + ' Modifying these words to:\n' + \
                sugg_words + '\n\n' + \
               '@what3words has identified a 3m square area on earth near: ' + '\n' + \
                emoji.emojize(':round_pushpin:') + nearest_loc + ' (' + country + ')' + '\n\n' + \
-              'What are your 3 words? \n\n' + \
-               emoji.emojize(':world_map:') + '  #what3words'
+              emoji.emojize(':house:') + ' What is your 3 word address? \n\n' + \
+               emoji.emojize(':world_map:') + ' #what3words'
 
+    print(status)
+
+    logger.info('Post to Twitter')
     # Upload Image
     # media = t_api.media_upload(file_name)
 
@@ -121,9 +138,9 @@ def twitter_post(t_api, file_name, trd_words, sugg_words, nearest_loc , country,
     # t_api.update_status(status=status, media_ids=[media.media_id], lat=latitude, long=longitude)
 
 
-
 def main(loc):
 
+    logger.info('Generating API keys')
     t_api = create_twitter_api()
     g_api = create_google_api()
     w_api = create_w3w_api()
@@ -132,6 +149,7 @@ def main(loc):
     # while not ticker.wait(WAIT_SECONDS):
 
     # Post to Twitter
+    logger.info('Execution begins here')
     trd_words = get_trending(t_api, loc)
     sugg_words, nearest_loc, country, latitude, longitude = get_loc(w_api, trd_words)
     file_name = map_generator(g_api, latitude, longitude)
@@ -141,6 +159,7 @@ def main(loc):
     now = datetime.now()
     dt_str = now.strftime("%m/%d/%Y %H:%M:%S")
     logger.info('Tweet posted on: ' + str(dt_str))
+    logger.info('Done.')
 
 
 if __name__ == "__main__":
